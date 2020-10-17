ee8588980488f60fe9d3b18dee37f7756ab9c322
murakamiyuuki <bbac8097@gmail.com>
2020-10-17 10:28:32 +0000

    課題２を追加

---
+++ kadai-02/index.js
@@ -0,0 +1,17 @@
+      const firstName = "yuki"
+      const familyName = "Murakami"
+
+      console.log(firstName + familyName)
+
+      // 1から10までの数字の和を表示する
+      console.log(1+2+3+4+5+6+7+8+9+10)
+
+      //真偽型の演算
+      const dog = false
+      const cat = true
+      //犬または猫を飼っている
+      console.log(dog || cat )
+      //犬と猫の両方を飼っている
+      console.log(dog && cat)
+      //犬も猫も飼っていない
+      console.log(!dog && !cat)
