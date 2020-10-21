17914bb67b7678403d3aff8348b3dba375494b4a
murakamiyuuki <bbac8097@gmail.com>
2020-10-21 05:31:28 +0000

    fix

---
+++ kadai-03/index1.js
@@ -0,0 +1,10 @@
+// 生まれた日を入力
+const day = parseInt(process.argv[2])
+
+if (day % 3 ===0) {
+    console.log('あなたは穏やかな性格です')
+} else if (day % 3 ===1) {
+    console.log('あなたは怒りっぽい性格です')
+} else {
+    console.log('あなたは冷酷な性格です')
+}
