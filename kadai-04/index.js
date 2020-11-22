8c478da19f73c40555acb976020738ae55bd969c
murakamiyuuki <bbac8097@gmail.com>
2020-11-22 22:14:16 +0000

    課題4を追加

--- kadai-04/index.js
+++ kadai-04/index.js
@@ -1,7 +1,13 @@
 const product = (a, b) => {
     return a * b
 }
+const sum = (n) => {
+    let result = 0
+    for (let index = 1; index < n +1; index = index +1) {
+        result = result + index
+    }
+    return result
+}
 
- console.log(product(1001,1005))
-
- const product = (1)
+console.log(product(107, 109))
+console.log(sum(500))
