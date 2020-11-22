fd399f3b5a32c8aa39bedc08671733543d3254e9
murakamiyuuki <bbac8097@gmail.com>
2020-11-22 21:46:49 +0000

    課題5を追加

---
+++ kadai-04/index.js
@@ -0,0 +1,7 @@
+const product = (a, b) => {
+    return a * b
+}
+
+ console.log(product(1001,1005))
+
+ const product = (1)
---
+++ kadai-05/index.js
@@ -0,0 +1,9 @@
+const profile = { 
+    私のnameは :"村上雄基です", 
+    私のageは :"20です",
+    私のhobbyは: "本を読むことです",
+    私のmajorは:"日本史学専攻です"
+
+}
+
+ console.log(profile)
